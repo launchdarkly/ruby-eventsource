86d34673f7e5336670dd78ce884b855984674444
Eli Bishop <eli@launchdarkly.com>
2019-01-03 14:41:27 -0800

    indents

--- lib/ld-eventsource.rb
+++ lib/ld-eventsource.rb
@@ -5,10 +5,10 @@ require "ld-eventsource/version"
 # A client for the Server-Sent Events protocol.
 #
 module LaunchDarklySSE
-	#
-	# Internal components of the SSE implementation. All classes in this module should be
-	# considered unsupported and subject to change.
-	#
-	module Impl
-	end
+  #
+  # Internal components of the SSE implementation. All classes in this module should be
+  # considered unsupported and subject to change.
+  #
+  module Impl
+  end
 end
